e4a5df32a7ee34caf7f2fd270ace3a22a575bc1a
gui.massoqueto <gui.massoqueto@gmail.com>
2022-06-28 16:41:12 -0300

    apdate: auth middleware for authenticated pages

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { getUserLogin, postUserLogin } from "../controllers/auth/user-login.controller";
 import { postUserLogout } from "../controllers/auth/user-logout.controller";
 import { getUserSignUp, postUserSignUp } from "../controllers/auth/user-signup.controller";
+import { onlyAuthAccess } from "../middlewares/setDefaultUser.middleware";
 
 const authRoute: Router = Router();
 
 authRoute.get('/login', getUserLogin);
 authRoute.post('/login', postUserLogin);
-authRoute.post('/logout', postUserLogout);
+authRoute.post('/logout', onlyAuthAccess, postUserLogout);
 authRoute.get('/signup', getUserSignUp);
 authRoute.post('/signup', postUserSignUp);
 
--- src/routes/shop.route.ts
+++ src/routes/shop.route.ts
@@ -5,17 +5,18 @@ import { getCart } from "../controllers/shop/get-cart.controller";
 import { postAddToCart } from "../controllers/shop/add-to-cart.controller";
 import { postRemoveFromCart } from "../controllers/shop/remove-from-cart.controller";
 import { postNewOrder, getNewOrder } from "../controllers/shop/new-order.controller";
+import { onlyAuthAccess } from "../middlewares/setDefaultUser.middleware";
 
 const shopRoute: Router = Router();
 
 shopRoute.get('/all-products', getAllProducts);
 shopRoute.get('/product/:id', getSingleProduct);
-shopRoute.post('/product/:id', postAddToCart);
+shopRoute.post('/product/:id', onlyAuthAccess, postAddToCart);
 
-shopRoute.get('/cart', getCart);
-shopRoute.post('/cart', postRemoveFromCart);
+shopRoute.get('/cart', onlyAuthAccess, getCart);
+shopRoute.post('/cart', onlyAuthAccess, postRemoveFromCart);
 
-shopRoute.get('/new-order', getNewOrder);
-shopRoute.post('/new-order', postNewOrder);
+shopRoute.get('/new-order', onlyAuthAccess, getNewOrder);
+shopRoute.post('/new-order', onlyAuthAccess, postNewOrder);
 
 export { shopRoute };
